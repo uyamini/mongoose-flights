7867185dddb5bbc12b7460c081555a0664d8dd35
uyamini <uyamini1996@gmail.com>
2024-04-03 18:06:20 -0500

    add tickets working

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -5,25 +5,24 @@ const Flight = require('../models/flight');
 // Define the 'new' method to show a form for creating a new ticket
 exports.new = async (req, res) => {
     try {
-      // Just pass the flightId to the view
-      const flightId = req.params.flightId; 
+      const flightId = req.params.id; // Changed from 'flightId' to 'id'
       res.render('tickets/new', { title: 'New Ticket', flightId });
     } catch (err) {
       console.error(err);
       res.redirect('/flights');
     }
-  };
+};
 
 // Function to create a new ticket for a flight
 exports.create = async (req, res) => {
     try {
-      req.body.flight = req.params.flightId; // Assign flightId from URL params to the ticket
+      req.body.flight = req.params.id; // Assign flightId from URL params to the ticket
       const ticket = new Ticket(req.body);
       await ticket.save();
-      res.redirect(`/flights/${req.params.flightId}`); // Redirect to the flight's detail page
+      res.redirect(`/flights/${req.params.id}`); // Redirect to the flight's detail page
     } catch (error) {
       console.error('Error creating ticket:', error);
-      res.redirect(`/flights/${req.params.flightId}`);
+      res.redirect(`/flights/${req.params.id}`);
     }
   };
 
